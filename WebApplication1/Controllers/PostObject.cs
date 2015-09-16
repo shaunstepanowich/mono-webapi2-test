5422828883ad770f9ac50159449dca2fb6e30fbd
Shaun Stepanowich <shauns@refineco.com>
2015-09-16 09:06:52 -0600

    initial commit

---
+++ WebApplication1/Controllers/PostObject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace WebApplication1
+{
+    public class PostObject
+    {
+        public PostObject ()
+        {
+        }
+
+        public string Email {get;set;}
+        public string Password {get;set;}
+    }
+}
+
---
+++ WebApplication1/Controllers/TestController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplication1.Controllers
+{
+    public class TestController : ApiController
+    {
+
+        [HttpGet]
+        public string GetTest()
+        {
+            return "test";
+        }
+
+        [HttpPost]
+        public string PostTest([FromBody]PostObject hello)
+        {
+            return "Sucess!";
+        }
+
+    }
+}
